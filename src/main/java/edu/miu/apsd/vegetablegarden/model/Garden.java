4aed4704e4126fa2f21bc402fa40e8f719177805
sanjeevthapamiu <sanjeev.thapa@miu.edu>
2024-11-05 10:16:40 -0600

    Create Entities

--- src/main/java/edu/miu/apsd/vegetablegarden/model/Garden.java
+++ src/main/java/edu/miu/apsd/vegetablegarden/model/Garden.java
@@ -1,4 +1,51 @@
 package edu.miu.apsd.vegetablegarden.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "gardens")
 public class Garden {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    @NotBlank(message = "Garden Name must not be blank")
+    @Column(nullable = false)
+    private String name;
+
+    private Double size;
+
+    @OneToMany(mappedBy = "garden", cascade = CascadeType.ALL)
+    private List<Plant> plants;
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", Name:'" + name +
+                ", Size: " + size + " m^2\n";
+    }
+
+
+
+    public Garden(String name, Double size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public void addPlant(Plant plant) {
+        plant.setGarden(this);
+        plants.add(plant);
+    }
 }
--- src/main/java/edu/miu/apsd/vegetablegarden/model/Plant.java
+++ src/main/java/edu/miu/apsd/vegetablegarden/model/Plant.java
@@ -1,4 +1,49 @@
 package edu.miu.apsd.vegetablegarden.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "plants")
 public class Plant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+
+    private String name;
+
+    private String type;
+
+    private LocalDate plantDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "garden_id")
+    private Garden garden;
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", Name:" + name +
+                ", Type: " + type +
+                ", Plant Date: " + plantDate +
+                ", Garden: " + garden.getName() + "\n";
+    }
+
+
+
+    public Plant(String name, String type, LocalDate plantDate) {
+        this.name = name;
+        this.type = type;
+        this.plantDate = plantDate;
+    }
 }
